f625aa21d930a42a14bbb8952b7f7577f93fdf8b
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2025-11-04 18:58:10 +0400

    model and modelview components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
+import { Model } from './components/Model'
 import Navbar from './components/Navbar'
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <Navbar/>
         <Hero/>
         <Highlights/>
+        <Model/>
       </main>
     </>
   )
