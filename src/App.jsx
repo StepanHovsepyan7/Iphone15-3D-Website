299060eee603e41d43766f2092fdadc5e00e4033
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2025-11-05 23:03:18 +0400

    Feature section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Featues from './components/Featues'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
 import { Model } from './components/Model'
@@ -13,6 +14,7 @@ const App = () => {
         <Hero/>
         <Highlights/>
         <Model/>
+        <Featues/>
       </main>
     </>
   )
